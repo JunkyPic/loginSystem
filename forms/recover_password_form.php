4dc4e3fe3f8e73a8de1d4da43af8680e1918b6a8
Andrei Popa <junkypic@gmail.com>
2014-08-31 05:22:46 +0300

    ta

--- forms/recover_password_form.php
+++ forms/recover_password_form.php
@@ -1,19 +1,24 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 <!--Yes, yes, using tables is so last 10 years. HTML is not the purpose here. PHP is.-->
     <head>
-        <title> Recover Password </title>
+        <title> Please enter email address </title>
+		<link href="css/style.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
-        <form action="lost_password.php" method="post">
+	<div id="header">
+        <h3>Login</h3>
+    </div>
+	<div id="wrap">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <table>
                 <tr>
-                    <td>Please enter email address:</td>
                     <td><input type="text" name="email"/>
                     <td><input type="submit" name="recoverPassword" value="Recover password"/>
                 </tr>
             </table>
             <p><a href="index.php">Login</a></p>
         </form>
+	</div>
     </body>
 </html>
