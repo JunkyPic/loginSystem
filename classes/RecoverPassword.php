2b46f5f622fe59f819ad4941aabfd00f6516ad9d
Andrei Popa <junkypic@gmail.com>
2014-09-03 00:17:20 +0300

    Update RecoverPassword.php

--- classes/RecoverPassword.php
+++ classes/RecoverPassword.php
@@ -21,36 +21,28 @@ class RecoverPassword
         
             require_once 'ValidateData.php';
             $validateData = new ValidateData();
-            
-            require_once 'db/db_connect.php';
-            require_once 'db/db_tables.php';
                 
             if($validateData->validateEmail($_POST['email'])){
                 $email = $_POST['email'];
 
-                /**
-                * Note that the variables used here
-                * come from the db_tables.php file
-                * not from user input
-                */
-                $sqlQuery = $dbPDO->prepare("SELECT $loginEmail FROM $tableName WHERE $loginEmail= :email");
-                $sqlQuery->execute(array(':email' => $email));
+                require_once 'SqlQueries.php';
+                $sqlQueries = new SqlQueries();
                 
-                $doesEmailExist = $sqlQuery->fetch(PDO::FETCH_ASSOC);
+                $doesEmailExist = $sqlQueries->selectLoginEmailForRecoverPassword($email);
                 
                 /**
-				* Does the email exist?
-				*/
+		* Does the email exist?
+		*/
                 if($doesEmailExist){
                     //echo $doesEmailExist['login_email'];
                     echo '<p>The email exists. Currently the recover password email will not send due to not having a proper SMPT configured.</p>';
-					/**
-					* TODO: Make this work. Configure SMTP server -> SwitfMail
-                    * Swift Mailer is downloaded and configured now.
-                    * Uncomment this when I get a chance to test
-                    * require_once 'SendMailRecoverPassword.php';
-                    * $swift = new RecoverPasswordFunctions();
-					*/
+			/**
+			* TODO: Make this work. Configure SMTP server -> SwitfMail
+			* Swift Mailer is downloaded and configured now.
+			* Uncomment this when I get a chance to test
+			* require_once 'SendMailRecoverPassword.php';
+			* $swift = new RecoverPasswordFunctions();
+			*/
                     
                 } else {
                     echo '<p>Email doesn\'t exist!</p>';
