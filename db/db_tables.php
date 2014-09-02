3ecdf7171e246ad680894686da92d63dd49e457c
Andrei Popa <junkypic@gmail.com>
2014-09-03 00:20:40 +0300

    Delete db_tables.php

--- db/db_tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-$tableName          = 'login_table';
-
-$loginId            = 'login_id';
-$loginUsername      = 'login_username';
-$loginPassword      = 'login_password';
-$loginEmail         = 'login_email';
-
