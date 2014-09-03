d0b919583a841b46c3ba485bf0b36864247fd1fb
Andrei Popa <junkypic@gmail.com>
2014-09-03 03:34:51 +0300

    03-Sep-2014

--- classes/RecoverPassword.php
+++ classes/RecoverPassword.php
@@ -31,18 +31,18 @@ class RecoverPassword
                 $doesEmailExist = $sqlQueries->selectLoginEmailForRecoverPassword($email);
                 
                 /**
-		* Does the email exist?
-		*/
+				* Does the email exist?
+				*/
                 if($doesEmailExist){
                     //echo $doesEmailExist['login_email'];
                     echo '<p>The email exists. Currently the recover password email will not send due to not having a proper SMPT configured.</p>';
-			/**
-			* TODO: Make this work. Configure SMTP server -> SwitfMail
-			* Swift Mailer is downloaded and configured now.
-			* Uncomment this when I get a chance to test
-			* require_once 'SendMailRecoverPassword.php';
-			* $swift = new RecoverPasswordFunctions();
-			*/
+					/**
+					* TODO: Make this work. Configure SMTP server -> SwitfMail
+                    * Swift Mailer is downloaded and configured now.
+                    * Uncomment this when I get a chance to test
+                    * require_once 'SendMailRecoverPassword.php';
+                    * $swift = new RecoverPasswordFunctions();
+					*/
                     
                 } else {
                     echo '<p>Email doesn\'t exist!</p>';
