7b8fc6d3be8ede7923fd969670c3c474a5df909e
Andrei Popa <junkypic@gmail.com>
2014-09-15 21:27:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 /**
 * Author: Andrei Popa
-* link: https://github.com/JunkyPic/loginSystem/tree/Test
-* 
-*A few things to note and some TODOs:
-*
-*    SOON - Trademark - See this link for clarification - http://www.wowwiki.com/Soon
-*    TODO: Split up classes. As it stands they are too bloated with functions. One class does way more than it needs to.
-*    TODO: The Recover Password does not work for now. I intend on using SwiftMail to do that. I never got around to setting up a proper SMTP.
-*
+* link: https://github.com/JunkyPic/loginSystem
 */
 
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
