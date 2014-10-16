aab274d83b5d73df61992f4ad5e6dd48e8c77ec8
Andrei Popa <junkypic@gmail.com>
2014-10-16 17:42:15 +0300

    Update lost_password.php

--- lost_password.php
+++ lost_password.php
@@ -1,8 +1,22 @@
 <?php
 require 'views/recover_password_form.php';
 require 'classes/RecoverPassword.php';
+require_once 'classes/ValidateCredentials.php';
 
-/**
-* The construct will take care of everything
-*/
-$recoverPassword = new RecoverPassword();
+if(isset($_POST['recoverPassword'])){
+
+    $credentials = (array('email' => $_POST['email']
+                           )
+                    );
+    
+    $validateLostPassword = new ValidateCredentials($credentials, 'recoverpassword');
+    
+    if(($validateLostPassword->doValidate()) != FALSE){
+        $credentials = $validateLostPassword->doValidate();
+        
+        $recoverPassword = new RecoverPassword($credentials);
+        $recoverPassword->doRecoverPassword();
+    }
+}
+
+
