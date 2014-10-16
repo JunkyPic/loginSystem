8992798d734fe1d696c81c3e9419c2c54422febd
Andrei Popa <junkypic@gmail.com>
2014-10-16 17:42:31 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,7 +1,24 @@
 <?php
 require_once 'classes/Register.php';
+require_once 'views/register_form.php';
+require_once 'classes/ValidateCredentials.php';
 
-/**
-* The construct in Register will take care of everything
-*/
-$register = new Register();
+if(isset($_POST['register'])){
+
+    $credentials = (array('username'      => $_POST['username'],
+                           'password'      => $_POST['password'],
+                           'passwordAgain' => $_POST['passwordAgain'],
+                           'email'         => $_POST['email']
+                            )
+                    );
+                                                    
+    $validateRegister = new ValidateCredentials($credentials, 'register');
+    
+    if(($validateRegister->doValidate()) != FALSE){
+        $credentials = $validateRegister->doValidate();
+        
+        $register = new Register($credentials);
+        $register->doRegister();
+    }
+}
+
