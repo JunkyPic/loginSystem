7b63dd4d29df32f2bf5b384659fae00594f04934
Andrei Popa <junkypic@gmail.com>
2014-10-16 17:41:27 +0300

    Update change_password.php

--- change_password.php
+++ change_password.php
@@ -4,9 +4,26 @@ session_start();
 if(isset($_SESSION['id'])){
 	include 'views/change_password_form.php';
 	require_once 'classes/ChangePassword.php';
-	$changePassword = new ChangePassword();
+    require_once 'classes/ValidateCredentials.php';
+    
+    if(isset($_POST['resetPassword'])){
+
+        $credentials = (array('passwordCurrent' => $_POST['passwordCurrent'],
+                               'passwordNew'     => $_POST['passwordNew'],
+                               'passwordNewAgain'=> $_POST['passwordNewAgain']
+                              )
+                        );
+                                                            
+        $validateLogin = new ValidateCredentials($credentials, 'resetpassword');
+        
+        if(($validateLogin->doValidate()) != FALSE){
+            $credentials = $validateLogin->doValidate();
+            
+            $ChangePassword = new ChangePassword($credentials);
+            $ChangePassword->doResetPassword();
+        }
+    }
+                                                    
 } else {
-	
-    //if session is not set redirect
     header('Location: index.php' . $redirect);
 }
