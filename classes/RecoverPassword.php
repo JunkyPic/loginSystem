ba2e091972cb778da88b3aeee3f3de6b2eee1f80
Andrei Popa <junkypic@gmail.com>
2014-10-16 17:43:21 +0300

    Update RecoverPassword.php

--- classes/RecoverPassword.php
+++ classes/RecoverPassword.php
@@ -1,83 +1,74 @@
 <?php
 
 class RecoverPassword{
-
-    public function __construct(){
+    
+    private $credentials;
+    
+    public function __construct($credentials){
     
         require_once 'PasswordHash.php';
         require_once 'SendMailRecoverPassword.php';
         require_once 'ValidateData.php';
         require_once 'SqlQueryController.php';
         
-        if(isset($_POST['recoverPassword'])){
-
-            $credentials = ValidateData::stripAllWhiteSpaces(array('email' => $_POST['email']
-                                                                   )
-                                                            );
-            $this->doRecoverPassword($credentials);
-        }
+        $this->credentials = $credentials;
+        
     }
     
-    public function doRecoverPassword($credentials){
+    public function doRecoverPassword(){
+
+        $sqlQueryController = new SqlQueryController();
         
-        if(ValidateData::validateEmail($credentials['email'])){
-
-            $sqlQueryController = new SqlQueryController();
+        $query = "SELECT login_email
+                  FROM users_table
+                  WHERE login_email=:email LIMIT 1";
+        $array = array(':email' => $this->credentials['email']);
+        
+        $emailExist = $sqlQueryController->executeQuery($query, $array, 'fetchAssoc');
+        
+        if($emailExist){
+        
+            /**
+            * If a proper SMTP is not configured, the password
+            * will not be changed and the page will die
+            * with a user friendly error.
+            * A more useful error can be found in the log fiels
+            * The __construct() of the class is built
+            * in such a way that it will throw the exception and die
+            * after. Point is, don't move this further down the page
+            * or the password WILL be changed but the email will NOT
+            * be sent if the SMTP is not configured!
+            */
+            $swift = new SendMailRecoverPassword();
             
-            $query = "SELECT login_email
-                      FROM users_table
+            $passwordHash = new PasswordHash();
+            
+            /*
+            * Create a random string of letteres and numbers
+            * This will be the users new password
+            */
+            $randomPassword = str_shuffle('abcdefghijklmnopqrstqwxz0123456789ABCDEFGHIJKLMNOPQRSTWXZ');
+            
+            $newPassword = $passwordHash->hashPassword($randomPassword);
+
+            $query = "UPDATE users_table
+                      SET login_password=:password
                       WHERE login_email=:email LIMIT 1";
-            $array = array(':email' => $credentials['email']);
+            $array = array(':password' => $newPassword,
+                           ':email'    => $this->credentials['email']);
+                           
+            $sqlQueryController->executeQuery($query, $array, 'execute');
             
-            $emailExist = $sqlQueryController->executeQuery($query, $array, 'fetchAssoc');
-            
-            if($emailExist){
-            
-                /**
-                * If a proper SMTP is not configured, the password
-                * will not be changed and the page will die
-                * with a user friendly error.
-                * A more useful error can be found in the log fiels
-                * The __construct() of the class is built
-                * in such a way that it will throw the exception and die
-                * after. Point is, don't move this further down the page
-                * or the password WILL be changed but the email will NOT
-                * be sent if the SMTP is not configured!
-                */
-                $swift = new SendMailRecoverPassword();
-                
-                $passwordHash = new PasswordHash();
-                
-                /*
-                * Create a random string of letteres and numbers
-                * This will be the users new password
-                */
-                $randomPassword = str_shuffle('abcdefghijklmnopqrstqwxz0123456789ABCDEFGHIJKLMNOPQRSTWXZ');
-                
-                $newPassword = $passwordHash->hashPassword($randomPassword);
+            $swift->createMessage($randomPassword, $this->credentials['email']);  
 
-                $query = "UPDATE users_table
-                          SET login_password=:password
-                          WHERE login_email=:email LIMIT 1";
-                $array = array(':password' => $newPassword,
-                               ':email'    => $credentials['email']);
-                               
-                $sqlQueryController->executeQuery($query, $array, 'execute');
-                
-                $swift->createMessage($randomPassword, $credentials['email']);  
-
-                $message = $swift->getMessage();
-                
-                if($swift->sendMessage($message)){
-                    echo '<p>Check your inbox for the new password. Your old password will no longer work</p>';
-                }
-                
-            } else {
-                echo '<p>Email doesn\'t exist!</p>';
+            $message = $swift->getMessage();
+            
+            if($swift->sendMessage($message)){
+                echo '<p>Check your inbox for the new password. Your old password will no longer work</p>';
             }
-              
+            
         } else {
-            echo '<p>Email is invalid</p>';
+            echo '<p>Email doesn\'t exist!</p>';
         }
 
     }
